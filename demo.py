6fbc90abee698e470e8f5523fa3bc8c9ec218ffe
pr-riad <54750259+pr-riad@users.noreply.github.com>
2023-03-31 20:21:58 +0000

    Update demo.py

--- demo.py
+++ demo.py
@@ -3,7 +3,13 @@ from sklearn.datasets import make_blobs
 from mec import mec_eval
 
 
-n_samples = 1500
-random_state = 170
-X, _ = make_blobs(n_samples=n_samples, random_state=random_state)
+n_samples = 1000
+random_state = 41
+n_centers = 8
+X, y = make_blobs(n_samples=n_samples, centers = n_centers, random_state=random_state)
 
+k = mec_eval(X, k_min = 2, k_max = 10, disp = True) 
+
+print()
+print("predecte n_cluster = ", k)
+print("GT n_cluster = ", len(np.unique(y)))
